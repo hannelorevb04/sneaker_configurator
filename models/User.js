740b4166693e932d8829809c4509d46e7b88c2b1
Amybr01 <148042817+Amybr01@users.noreply.github.com>
2024-12-08 02:07:40 +0100

    fix

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 
 // Definieer het schema voor een gebruiker
 const userSchema = new mongoose.Schema({
